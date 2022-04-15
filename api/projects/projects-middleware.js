4cfc258b80849756d31f939f2c6e99270bdc6476
Joshmclemore <josh_mclemore@yahoo.com>
2022-04-15 15:02:20 -0500

    17 tests passing

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,42 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model');
+
+function ensureIdExists(req, res, next) {
+    Projects.get(req.params.id)
+        .then(project => {
+            if(!project) {
+                res.status(404).json({
+                    message: "Project not found"
+                })
+            } else {
+                req.existingId = project;
+                next();
+            }
+        })
+}
+
+function checkforMissingInfo (req, res, next) {
+    const {name, description, completed} = req.body
+    if(!name || name.trim() === "" || !description || description.trim() === "") {
+        res.status(400).json({
+            message: "Please include a valid name and a description in your post"
+        })
+    } else {
+        req.project = { completed: req.body.completed, name: req.body.name.trim(), description: req.body.description.trim() };
+        next();
+    }
+}
+
+function checkforMissingInfoPut (req, res, next) {
+    const {name, description, completed} = req.body
+    if(!name || name.trim() === "" || !description || description.trim() === "" || typeof completed != 'boolean' ) {
+        res.status(400).json({
+            message: "Please include a valid name, description, and completed status in your post"
+        })
+    } else {
+        req.project = { completed: req.body.completed, name: req.body.name.trim(), description: req.body.description.trim() };
+        next();
+    }
+}
+
+module.exports = { ensureIdExists, checkforMissingInfo, checkforMissingInfoPut }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,86 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const Projects = require('./projects-model');
+const { ensureIdExists, checkforMissingInfo, checkforMissingInfoPut } = require('./projects-middleware');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            res.status(err.status || 500).json({
+                message: 'Error retrieving the projects',
+                stack: err.stack,
+              });
+        })
+})
+
+router.get('/:id', ensureIdExists, (req, res) => {
+    Projects.get(req.params.id)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            res.status(err.status || 500).json({
+                message: 'Error retrieving the project',
+                stack: err.stack,
+            });
+        })
+})
+
+router.post('/', checkforMissingInfo, (req, res) => {
+    Projects.insert(req.project)
+        .then(verifiedInfo => {
+            res.json(verifiedInfo)
+        })
+        .catch(err => {
+            res.status(err.status || 500).json({
+                message: 'Error adding your post to the database',
+                stack: err.stack,
+            });
+        })
+})
+
+router.put('/:id', checkforMissingInfoPut, (req, res) => {
+    Projects.update(req.params.id, req.project)
+        .then(verifiedInfo => {
+            res.json(verifiedInfo)
+        })
+        .catch(err => {
+            res.status(err.status || 500).json({
+                message: 'Error updating your post in the database',
+                stack: err.stack,
+            });
+        })
+})
+
+router.delete('/:id', ensureIdExists, (req, res) => {
+    Projects.remove(req.params.id)
+        .then(() => {
+            res.json(null)
+        })
+        .catch(err => {
+            res.status(err.status || 500).json({
+                message: 'Error deleting your post from the database',
+                stack: err.stack,
+            });
+        })
+})
+
+router.get('/:id/actions', ensureIdExists, (req, res) => {
+    Projects.getProjectActions(req.params.id)
+        .then(array => {
+            res.status(200).json(array)
+        })
+        .catch(err => {
+            res.status(err.status || 500).json({
+                message: 'Error retrieving the array of the selected post',
+                stack: err.stack,
+            });
+        })
+})
+module.exports = router;
